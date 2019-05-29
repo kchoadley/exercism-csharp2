e1ec374249ce78dbc46ca26ead67119f2bd3a32d
Kristofer Hoadley <kristoferhoadley@gmail.com>
2019-05-29 10:16:26 -0700

    Initial solution two-fer

--- two-fer/TwoFer.cs
+++ two-fer/TwoFer.cs
@@ -1,7 +1,4 @@
 ﻿public static class TwoFer
 {
-    public static string Speak()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static string Speak(string name = "you") => $"One for {name}, one for me.";
 }
