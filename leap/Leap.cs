e1d2f6bd3465294880e97db39c4ade34dfd88541
Kristofer Hoadley <kristoferhoadley@gmail.com>
2019-05-29 10:59:16 -0700

    Improved solution to leap.
    Borrowed idea from https://exercism.io/tracks/csharp/exercises/leap/solutions/cf5c078d2a8a2c187917775e

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -3,13 +3,7 @@ using System;
 public static class Leap
 {
     public static bool IsLeapYear(int year) =>
-        DivisibleBy4(year) ?
-            DivisibleBy100(year) ?
-                DivisibleBy400(year) ? true : false
-                : true
-        : false;
-    private static bool DivisibleBy4(int year) => year % 4 == 0;
-    private static bool DivisibleBy100(int year) => year % 100 == 0;
-    private static bool DivisibleBy400(int year) => year % 400 == 0;
+        year.IsDivisibleBy(4) && (!year.IsDivisibleBy(100) || year.IsDivisibleBy(400));
+    private static bool IsDivisibleBy(this int number, int divisor) => number % divisor == 0;
 
 }
