34f29b7584296ed156dddcf8d29b5710a1ff0a69
Kristofer Hoadley <kristoferhoadley@gmail.com>
2019-05-29 10:44:10 -0700

    Initial solution to leap

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,8 +2,14 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsLeapYear(int year) =>
+        DivisibleBy4(year) ?
+            DivisibleBy100(year) ?
+                DivisibleBy400(year) ? true : false
+                : true
+        : false;
+    private static bool DivisibleBy4(int year) => year % 4 == 0;
+    private static bool DivisibleBy100(int year) => year % 100 == 0;
+    private static bool DivisibleBy400(int year) => year % 400 == 0;
+
 }
