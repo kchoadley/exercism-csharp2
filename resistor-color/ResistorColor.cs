3b1adfe4cd38f6581a0f76d4176af3d6f766bf49
Kristofer Hoadley <kristoferhoadley@gmail.com>
2019-06-02 10:34:42 -0700

    Initial solution to resistor-color

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private static string[] ColorMap =
+    {
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
+    public static int ColorCode(string color) => Array.IndexOf(ColorMap, color);
+
+    public static string[] Colors() => ColorMap;
+}
