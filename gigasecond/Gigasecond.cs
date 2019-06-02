3a6e57957171b0e0f3dfd4c46d0501e057c52d36
Kristofer Hoadley <kristoferhoadley@gmail.com>
2019-06-02 09:52:08 -0700

    Initial solution to gigasecond

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -2,8 +2,7 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    // Timespan is measured in ticks of 100 Nano seconds.
+    // Gigasecond (10^9) converted to 100 Nanoseconds (10^-7) is 10^16
+    public static DateTime Add(DateTime moment) => moment.Add(new TimeSpan(10_000_000_000_000_000));
 }
